9b201ac421dcf30fbe5b2d3e0aa428694ec4df1e
Leon Cao <leoncao1991@gmail.com>
2025-04-01 17:30:37 +0800

    封装DateUtil

--- saihu/src/main/java/com/saihu/common/util/DateUtil.kt
+++ saihu/src/main/java/com/saihu/common/util/DateUtil.kt
@@ -4,24 +4,23 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-open class DateUtil {
-
-    open fun currentDate(): String {
+object DateUtil {
+    fun currentDate(): String {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.CHINA)
         return dateFormat.format(Date())
     }
 
-    open fun currentDateTime(): String {
+    fun currentDateTime(): String {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA)
         return dateFormat.format(Date())
     }
 
-    open fun format(dateMillis: Long): String {
+    fun format(dateMillis: Long): String {
         val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.CHINA)
         return sdf.format(Date(dateMillis))
     }
 
-    open fun format(timestampStr: String?): String {
+    fun format(timestampStr: String?): String {
         val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.CHINA)
         val outputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA)
         return try {
--- saihu/src/main/java/com/saihu/common/util/Global.kt
+++ saihu/src/main/java/com/saihu/common/util/Global.kt
@@ -3,9 +3,9 @@ package com.saihu.common.util
 import android.content.Context
 import com.saihu.common.data.model.Account
 
-open class Global {
+open class GlobalBase {
     private val TAG = "Global"
-    open var user: Account? = null
+    open var account: Account? = null
         set(value) {
             field = value
             SpUtil.saveAccount(field)
